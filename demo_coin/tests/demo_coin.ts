8d4fe9b7a23ef7cb82d0c67d48b773998534dea5
Vishal772-pixel <fantasydark001@gmail.com>
2025-07-23 15:22:59 +0530

    Implement code changes to enhance functionality and improve performance

--- demo_coin/tests/demo_coin.ts
+++ demo_coin/tests/demo_coin.ts
@@ -1,16 +1,44 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { DemoCoin } from "../target/types/demo_coin";
+// import { DemoCoin } from "../target/types/demo_coin";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, getAssociatedTokenAddress, createMint, mintTo } from "@solana/spl-token";
 
-describe("demo_coin", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("demo_coin",()=>{
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.demoCoin as Program<DemoCoin>;
+  const program = anchor.workspace.DemoCoin as Program;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Intialize  mint and mint tokeds", async ()=>{
+    const mintKeypair = anchor.web3.Keypair.generate();
+    const authority = provider.wallet;
+
+    // Derive ATA (associated Token Account ) for user
+    const recipientTokenAccount = await getAssociatedTokenAddress
+    (
+      mintKeypair.publicKey,
+      authority.publicKey
+    );
+
+
+   // Call intialize_mint
+
+   await program.methods
+   .intializeMint(new anchor.BN(6)) // 6 decimels 
+   .accounts({
+    mint:mintKeypair.publicKey,
+    authority:authority.publicKey,
+    rent:anchor.web3.SYSVAR_RENT_PUBKEY,
+    systemProgram:SystemProgram.programId,
+    tokenProgram:TOKEN_PROGRAM_ID,
+   })
+   .signers([mintKeypair])
+   .rpc();
+
+
+console.log("Token Minted to :",recipientTokenAccount.toBase58());
+
+
   });
 });
